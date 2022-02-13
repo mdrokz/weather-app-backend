fa79a574c6cbfeac6c13e268b1d99a4772eee186
mdrokz <33598169+mdrokz@users.noreply.github.com>
2022-02-13 23:27:30 +0530

    implement multer

--- index.js
+++ index.js
@@ -1,21 +1,31 @@
 'use strict'
 const express = require('express')
 
+const upload = multer({ dest: './images/' });
+
 // Create the express app
 const app = express()
+
+const Router = express.Router;
 
 // Routes and middleware
 // app.use(/* ... */)
 // app.get(/* ... */)
 
 // Error handlers
-app.use(function fourOhFourHandler (req, res) {
-  res.status(404).send()
-})
-app.use(function fiveHundredHandler (err, req, res, next) {
-  console.error(err)
-  res.status(500).send()
-})
+
+const router = new Router();
+
+router.post('/upload', upload.single('file'), function (req, res) {
+
+  console.log(req.file, res);
+
+});
+
+
+
+
+app.use('/api', router);
 
 // Start server
 app.listen(8080, function (err) {
